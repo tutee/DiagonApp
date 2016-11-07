7531f93d49a64a94630404507d52a2d56b7e8698
Tute <tute.guazzaroni@gmail.com>
2016-11-06 22:55:09 -0300

    6/11
    Adaptado el login a google en la actual navegacion, falta la parte logica en nuestra base al momento del log

--- app/src/main/java/com/rightdecisions/diagonapp/activities/MainActivity.java
+++ app/src/main/java/com/rightdecisions/diagonapp/activities/MainActivity.java
@@ -3,17 +3,11 @@ package com.rightdecisions.diagonapp.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.Window;
 import android.widget.Button;
 
 import com.rightdecisions.diagonapp.R;
-import com.rightdecisions.diagonapp.fragments.LoginFragment;
-import com.rightdecisions.diagonapp.fragments.RegisterFragment;
 
 /**
  * Created by Tute on 1/11/2016.
