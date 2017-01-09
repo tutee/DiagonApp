db443365117375d20feab897c88597875610e65f
Tute <tute.guazzaroni@gmail.com>
2017-01-09 17:00:13 -0300

    09/01
    Sitio expandido funcionando, falta formalizar todo el layout.
    
    Activity de mapas aun no establecido. Falta decidir colores (origen/destino) de la recycler

---
+++ app/src/main/java/com/rightdecisions/diagonapp/activities/AnimateToolbar.java
@@ -0,0 +1,127 @@
+package com.rightdecisions.diagonapp.activities;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.graphics.Palette;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.BitmapEncoder;
+import com.rightdecisions.diagonapp.R;
+
+import com.rightdecisions.diagonapp.model.VersionModel;
+
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AnimateToolbar extends AppCompatActivity {
+    CollapsingToolbarLayout collapsingToolbar;
+    RecyclerView recyclerView;
+    int mutedColor = R.attr.colorPrimary;
+    SimpleRecyclerAdapter simpleRecyclerAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_animate_toolbar);
+
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.anim_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        collapsingToolbar = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        collapsingToolbar.setTitle(Globales.SENombre);
+
+        ImageView header = (ImageView) findViewById(R.id.header);
+
+        Glide.with(this).load(Globales.SEImagen).into(header);
+
+        //Bitmap bitmap = getBitmapFromURL(Globales.SEImagen);
+        //header.setImageBitmap(bitmap);
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(),
+                R.drawable.header);
+
+        Palette.from(bitmap).generate(new Palette.PaletteAsyncListener() {
+            @SuppressWarnings("ResourceType")
+            @Override
+            public void onGenerated(Palette palette) {
+
+                mutedColor = palette.getMutedColor(R.color.colorAccent);
+                collapsingToolbar.setContentScrimColor(mutedColor);
+                collapsingToolbar.setStatusBarScrimColor(R.color.blue_grey_500);
+            }
+        });
+
+        recyclerView = (RecyclerView) findViewById(R.id.scrollableview);
+
+        recyclerView.setHasFixedSize(true);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        List<String> listData = new ArrayList<String>();
+        int ct = 0;
+            for (int i = 0; i < VersionModel.data.length * 2; i++) {
+            listData.add(VersionModel.data[ct]);
+            ct++;
+            if (ct == VersionModel.data.length) {
+                ct = 0;
+            }
+        }
+
+        if (simpleRecyclerAdapter == null) {
+            simpleRecyclerAdapter = new SimpleRecyclerAdapter(listData);
+            recyclerView.setAdapter(simpleRecyclerAdapter);
+        }
+
+    }
+
+
+
+
+    public static Bitmap getBitmapFromURL(String src) {
+        try {
+            URL url = new URL(src);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoInput(true);
+            connection.connect();
+            InputStream input = connection.getInputStream();
+            Bitmap myBitmap = BitmapFactory.decodeStream(input);
+            return myBitmap;
+        } catch (IOException e) {
+            // Log exception
+            return null;
+        }
+    }
+/*
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+            case R.id.action_settings:
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }*/
+}
